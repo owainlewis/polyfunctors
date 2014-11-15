0439ca5e243e969b78464ea03bd168de28c5961f
Owain Lewis <owain@owainlewis.com>
2014-11-15 08:09:45 +0000

    cleanup and finish instances for quad functor

--- src/Control/PolyFunctor.hs
+++ src/Control/PolyFunctor.hs
@@ -1,7 +1,27 @@
 {-# LANGUAGE TypeOperators #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.PolyFunctor
+-- Copyright   :  2014 Owain Lewis
+-- License     :  public domain
+--
+-- Maintainer  :  owain@owainlewis.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- A collection of experimental polyfunctor instances you probably don't need
+--
+-----------------------------------------------------------------------------
 
 module Control.PolyFunctor
-  ( ) where
+  ( TriFunctor(..)
+  , QuadFunctor(..)
+  , Tri(..)
+  , Quad(..)
+  ) where
+
+-----------------------------------------------------------------------------
+-- TriFunctor
 
 class TriFunctor f where
   trimap :: (a -> x) ->
@@ -10,20 +30,42 @@ class TriFunctor f where
             (f a b c -> f x y z)
 
 data Tri a b c =
-  TriA a |
-  TriB b |
-  TriC c
+    TriA a
+  | TriB b
+  | TriC c
 
 instance TriFunctor (,,) where
-  trimap f g h (x, y, z) = ((f x), (g y), (h z))
+    trimap f g h (x, y, z) = ((f x), (g y), (h z))
+    {-# INLINE trimap #-}
 
 instance TriFunctor Tri where
-    trimap f _ _ (TriA x) = (TriA (f x))
-    trimap _ g _ (TriB y) = (TriB (g y))
-    trimap _ _ h (TriC z) = (TriC (h z))
+    trimap f _ _ (TriA x) = TriA (f x)
+    trimap _ g _ (TriB y) = TriB (g y)
+    trimap _ _ h (TriC z) = TriC (h z)
+    {-# INLINE trimap #-}
+
+-----------------------------------------------------------------------------
+-- Quad Functor
+data Quad a b c d =
+    QuadA a
+  | QuadB b
+  | QuadC c
+  | QuadD d
 
 class QuadFunctor f where
-    quadmap :: (a -> a') -> (b -> b') -> (c -> c') -> (d -> d') -> (f a b c d -> f a' b' c' d')
+    quadmap :: (a -> a') ->
+               (b -> b') ->
+               (c -> c') ->
+               (d -> d') ->
+               (f a b c d -> f a' b' c' d')
 
 instance QuadFunctor (,,,) where
     quadmap f g h i (a, b, c, d) = ((f a), (g b), (h c), (i d))
+    {-# INLINE quadmap #-}
+
+instance QuadFunctor Quad where
+    quadmap f _ _ _ (QuadA a) = QuadA (f a)
+    quadmap _ g _ _ (QuadB b) = QuadB (g b)
+    quadmap _ _ h _ (QuadC c) = QuadC (h c)
+    quadmap _ _ _ i (QuadD d) = QuadD (i d)
+    {-# INLINE quadmap #-}
